8181ecf0c8dcb1baa7f11aea8f12902d4a32610f
Manojparm <123813997+Manojparm@users.noreply.github.com>
2023-07-29 20:59:50 +0530

    solution 9

---
+++ solution9.js
@@ -0,0 +1,21 @@
+The output of this question will be :
+
+array 1: length=4 last=n
+array 2: length=5 last=j,o,n,e,
+
+
+Explanation:
+
+var arr1="john.split("");
+it will split the string like this [j,o,h,n]
+
+var arr2 = arr1.reverse();
+it will reverse the [j,o,h,n] like this [n,h,o,j]
+
+var arr3 = "jones".split('');
+it will split "jones" string like this [j,o,n,e,s]
+
+arr2.push(arr3);
+
+it will push all the elements of array three in array 2.
+
